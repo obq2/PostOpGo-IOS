73bd51ba9016bb17b4f8784c15eddd9405b4e418
obq2 <33470231+obq2@users.noreply.github.com>
2017-11-09 22:07:48 -0500

    Add files via upload

---
+++ PostOpGo/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  PostOpGo
+//
+//  Created by Omar Q on 9/21/17.
+//  Copyright © 2017 GROQ Systems. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
